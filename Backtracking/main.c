fa1632e48f9f245f2f07c0070cabf1fc4d25cfa9
= <=>
2024-07-13 15:46:38 -0300

    Solução para o problema dos n numeros e n combinações de moedas

--- Backtracking/main.c
+++ Backtracking/main.c
@@ -52,13 +52,99 @@ void func1(char *v, int pos, int n)
         }
     }
 }
-void func2()
+void func2(int *v, int pos, int n, int *auxVet)
+// Todas as combinações de {15,31,55,44}, de tamanho n, cuja soma dos elementos da combinação é par
 {
-    printf("entre1 2");
+    if (pos == n)
+    {
+        int soma = 0;
+        for (int i = 0; i < n; i++)
+        {
+            soma += v[i];
+        }
+
+        if (soma % 2 == 0)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                printf("%d ", v[i]);
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        for (int index = 0; index <= 3; index++)
+        {
+            v[pos] = auxVet[index];
+            func2(v, pos + 1, n, auxVet);
+        }
+    }
 }
-void func3()
+void func3(int *moedas, int centavo, int n, int pos, int *auxVet, int startIndex)
 {
-    printf("entre1 3");
+    if (centavo == 0)
+    {
+        int c1, c5, c10, c25, c50;
+        c1 = 0;
+        c5 = 0;
+        c10 = 0;
+        c25 = 0;
+        c50 = 0;
+
+        for (int i = 0; i < pos; i++)
+        {
+            if (moedas[i] == 1)
+            {
+                c1++;
+            }
+            if (moedas[i] == 5)
+            {
+                c5++;
+            }
+            if (moedas[i] == 10)
+            {
+                c10++;
+            }
+            if (moedas[i] == 25)
+            {
+                c25++;
+            }
+            if (moedas[i] == 50)
+            {
+                c50++;
+            }
+        }
+
+        printf("Voce precisa de %d de 1, %d de 5, %d de 10, %d de 25, %d de 50\nOU\n", c1, c5, c10, c25, c50);
+        return;
+    }
+    if (centavo < 0 || pos == n)
+    {
+        return;
+    }
+
+    for (int index = startIndex; index < 5; index++)
+    {
+        moedas[pos] = auxVet[index];
+        func3(moedas, centavo - auxVet[index], n, pos + 1, auxVet, index);
+    }
+}
+
+int menu2(int option){
+    system("clear");
+    if(option == 0){
+
+        printf("Qual o N desejado?: ");
+        scanf(" %d", &option);
+        return option;
+
+    }
+    if(option == 1){
+        printf("Qual o valor em centavos desejado?: ");
+        scanf(" %d", &option);
+        return option;
+    }
 }
 
 void menu1()
@@ -79,17 +165,24 @@ void menu1()
         switch (entrada)
         {
         case 1:
-            char v[3] = {'a', 'e', 'i'};
-            func1(v, 0, 5);
+            char v0[3] = {'a', 'e', 'i'};
+            int n = menu2(0);
+            func1(v0, 0, n);
             printf("\n");
-            
+
             break;
         case 2:
-            func2();
+            int v1[4] = {15, 31, 55, 44};
+            int vAux[4] = {15, 31, 55, 44};
+            int n2 = menu2(0);
+            func2(v1, 0, n2, vAux);
             printf("\n");
             break;
         case 3:
-            func3();
+            int moedas[100];
+            int vAux1[5] = {1, 5, 10, 25, 50};
+            int centavo = menu2(1);
+            func3(moedas, centavo, 100, 0, vAux1, 0);
             printf("\n");
             break;
         case 4:
--- Backtracking/test.c
+++ Backtracking/test.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
-int main(int argc, char const *argv[])
+
+// Função recursiva para encontrar todas as combinações de moedas
+void func3(int *moedas, int centavo, int n, int pos, int *auxVet)
 {
-    
-    char vet[3] = {123, 23, 121};
-    printf("%c", vet[0]);
+    if (centavo == 0)
+    {
+        int c1 = 0, c5 = 0, c10 = 0, c25 = 0, c50 = 0;
+
+        for (int i = 0; i < pos; i++)
+        {
+            if (moedas[i] == 1) c1++;
+            else if (moedas[i] == 5) c5++;
+            else if (moedas[i] == 10) c10++;
+            else if (moedas[i] == 25) c25++;
+            else if (moedas[i] == 50) c50++;
+        }
+
+        printf("Voce precisa de %d de 1, %d de 5, %d de 10, %d de 25, %d de 50\n", c1, c5, c10, c25, c50);
+        return;
+    }
+
+    if (centavo < 0 || pos == n)
+        return;
+
+    for (int index = 0; index < 5; index++)
+    {
+        moedas[pos] = auxVet[index];
+        func3(moedas, centavo - auxVet[index], n, pos + 1, auxVet);
+    }
+}
+
+int main()
+{
+    int centavo;
+    printf("Digite o valor em centavos: ");
+    scanf("%d", &centavo);
+
+    int moedas[100];  // Array para armazenar as combinações
+    int auxVet[5] = {1, 5, 10, 25, 50};  // As denominações das moedas
+
+    // Chama a função recursiva
+    func3(moedas, centavo, 100, 0, auxVet);
+
     return 0;
 }
